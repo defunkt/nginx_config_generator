105da636469486f881ddba01214d075f9b897d59
Chris Wanstrath <chris@ozmm.org>
2007-04-24 08:44:51 +0000

    example

--- generate_nginx_config.rb
+++ generate_nginx_config.rb
@@ -3,10 +3,15 @@
 
 def error(message) !puts(message) && exit end
 
+if ARGV.include? '--example'
+  puts open(File.dirname(__FILE__) + '/config.yml.example').read
+  exit
+end
+
 env_in  = ENV['NGINX_CONFIG_YAML']
 env_out = ENV['NGINX_CONFIG_FILE']
 
-error "Usage: generate_nginx_config.rb [config file] [out file]" if ARGV.empty? && !env_in
+error "Usage: generate_nginx_config [config file] [out file]" if ARGV.empty? && !env_in
 
 config   = YAML.load_file(env_in || ARGV.shift || 'config.yml')
 template = 'nginx.erb'
