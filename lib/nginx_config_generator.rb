13338628acc485c05d408d0837a3cda08fdf1828
Chris Wanstrath <chris@ozmm.org>
2007-08-24 01:04:23 +0000

    nginx: fix borkd gem

--- lib/nginx_config_generator.rb
+++ lib/nginx_config_generator.rb
@@ -14,14 +14,14 @@ env_out = ENV['NGINX_CONFIG_FILE']
 
 error "Usage: generate_nginx_config [config file] [out file]" if ARGV.empty? && !env_in
 
-overwrite = !(%w(-y -o -f --force --overwrite) & ARGV).empty?
+overwrite = %w(-y -o -f --force --overwrite).any? { |f| ARGV.delete(f) }
 
-config   = YAML.load(ERB.new(env_in || ARGV.shift || 'config.yml').result)
+config   = YAML.load_file(ERB.new(env_in || ARGV.shift || 'config.yml').result)
 template = file:'nginx.erb'
 
 if File.exists?(out_file = env_out || ARGV.shift || 'nginx.conf') && !overwrite
   error "=> #{out_file} already exists, won't overwrite it.  Quitting."
 else
-  open(out_file, 'w+').write(ERB.new(File.read(template), nil, '>').result)
+  open(out_file, 'w+').write(ERB.new(File.read(template), nil, '>').result(binding))
   error "=> Wrote #{out_file} successfully."
 end
